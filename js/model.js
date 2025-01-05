ae61c5669dc3e0464610d30fa8d4373e782d314e
Serhii Hul <owna.sgul@gmail.com>
2025-01-05 15:11:18 +0200

    some changes

--- js/model.js
+++ js/model.js
@@ -1,35 +1,21 @@
 const contactPhoneModel = {
   contacts: [],
-  // favorites: [],
   recentCalls: [],
   blockedPhones: [],
   currentId: 101,
 
   addContact(contact) {
     if (!contact.firstName || !contact.secondName || !contact.phone) return
-    delete contact.id
-    const createdContact = Object.assign({ id: this.currentId++ }, contact)
     contact.isFavorite = false
+    const createdContact = Object.assign({ id: this.currentId++ }, contact)
     this.contacts.push(createdContact)
   },
 
   updateContactById(id, contact) {
-    const findedContact = this.contacts.find(contact => contact.id === id)
-    if (!findedContact) return
+    const foundContact = this.contacts.find(contact => contact.id === id)
+    if (!foundContact) return
     delete contact.id
-    Object.assign(findedContact, contact)
-  },
-
-  markFavoriteById(id) {
-    const contact = this.contacts.find(contact => contact.id === id)
-    if (!contact) return
-
-    const isFavoriteContact = this.favorites.some(
-      favorite => favorite.id === id
-    )
-    if (!isFavoriteContact) {
-      this.favorites.push(contact)
-    }
+    Object.assign(foundContact, contact)
   },
 
   markFavoriteById(id) {
@@ -42,27 +28,13 @@ const contactPhoneModel = {
     return this.contacts.filter(contact => contact.isFavorite)
   },
 
-  searchContact(query) {
-    const lowerQuery = query.toLowerCase()
-
-    return this.contacts.filter(contact => {
-      return (
-        contact.firstName.toLowerCase().includes(lowerQuery) ||
-        contact.secondName.toLowerCase().includes(lowerQuery) ||
-        contact.phone.includes(query)
-      )
-    })
-  },
-
   removeContactById(id) {
     this.removeContactFromAllLists(id)
   },
 
   removeContactFromAllLists(id) {
     this.contacts = this.contacts.filter(contact => contact.id !== id)
-    this.favorites = this.favorites.filter(favorite => favorite.id !== id)
     this.recentCalls = this.recentCalls.filter(call => call.id !== id)
-    this.blockedPhones = this.blockedPhones.filter(block => block.id !== id)
   },
 
   addToBlockedList(id) {
@@ -96,25 +68,14 @@ contactPhoneModel.addContact({
 
 console.log(contactPhoneModel.contacts)
 
-// contactPhoneModel.removeContactById(contactPhoneModel.contacts[0].id)
-// console.log(contactPhoneModel.contacts)
-
-contactPhoneModel.markFavoriteById(contactPhoneModel.contacts[0].id)
-console.log(contactPhoneModel.favorites)
-
 contactPhoneModel.updateContactById(103, {
   firstName: 'Мария',
   phone: '0442467222',
 })
 
-console.log(contactPhoneModel.editContact)
+console.log(contactPhoneModel.contacts)
 
-// const searchResultsByName = contactPhoneModel.searchContact('Ирина')
-// console.log(searchResultsByName)
-// console.log(contactPhoneModel.filteredContacts)
+contactPhoneModel.markFavoriteById(103)
+contactPhoneModel.getFavorites(103)
 
-contactPhoneModel.addToBlockedList(contactPhoneModel.contacts[0].id)
-console.log(contactPhoneModel.blockedPhones)
-
-contactPhoneModel.removeContactFromAllLists(contactPhoneModel.contacts[0].id)
-console.log(contactPhoneModel.blockedPhones)
+console.log(contactPhoneModel.contacts)
